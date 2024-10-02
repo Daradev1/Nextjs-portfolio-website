650db146cfa04c3e1b46a6bbd79c058f71d6f6a4
Lekiss <lekissdarams7@gmail.com>
2024-10-01 17:57:40 -0700

    update changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Dara's Portfolio",
+  description: "Explore Dara's Portfolio",
 };
 
 export default function RootLayout({
